f45e454e9c43cf687515b1a43955bdad33407e54
lifeng <506076395@qq.com>
2022-03-22 00:29:31 +0800

    SVG：使用 svg-spriter-loader 引入 svg

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,35 @@
 <template>
 <div>
   <div>
-    <router-link to="/ledger">记账</router-link>
-    <router-link to="/labels">标签</router-link>
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/ledger">
+      <svg>
+        <use xlink:href="#ledger"/>
+      </svg>
+      记账
+    </router-link>
+    |
+    <router-link to="/labels">
+      <svg>
+        <use xlink:href="#labels"/>
+      </svg>
+      标签
+    </router-link>
+    |
+    <router-link to="/statistics">
+      <svg>
+        <use xlink:href="#statistics"/>
+      </svg>
+      统计
+    </router-link>
   </div>
 </div>
 </template>
 
 <script lang="ts">
+import x from '@/assets/icons/ledger.svg'
+import y from '@/assets/icons/labels.svg'
+import z from '@/assets/icons/statistics.svg'
+console.log(x,y,z);
 export default {
 name: "Nav"
 }
