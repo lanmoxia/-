95bf1faa9ac8b063f5b42a2c0c516ee6df2c5b75
lifeng <506076395@qq.com>
2022-03-22 16:41:33 +0800

    svg-sprite-loader-mod 解决 sass 引入 webstorm 提示问题

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,14 @@ module.exports = {
         .test(/\.svg$/) //规则特点 能匹配这里的正则就能使用
         .include.add(dir).end() // 只需要 dir 这个目录走这个规则
         // 配置 loader
-        .use('svg-sprite-loader-loader').loader('svg-sprite-loader').options({extract: false}).end()
+        // 1. 所有 loader 后边添加 -mod 的是修复 sass 引入问题 yarn add --dev svg-sprite-loader-mod
+        // 2. webstorm 设置 搜索 webpack 添加文件  目录：D:\demo\ledger\node_modules\@vue\cli-service\webpack.config.js
+        .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
         // 下边两句是消除 icon 默认颜色
         .use('svg-loader').loader('svgo-loader')
         .tap(options =>({...options, plugins:[{removeAttrs:{attrs:'fill'}}]})).end()
     //配置插件
-    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
+    config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
     config.module.rule('svg').exclude.add(dir) // 其他 SVG loader 排除 icons 目录
   }
 }
