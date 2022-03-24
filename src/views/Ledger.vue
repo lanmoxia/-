fc3b5a985c6c9d01c6b82fd6d4f30bdbb8218d95
lifeng <506076395@qq.com>
2022-03-25 00:04:29 +0800

    改为TS

--- src/views/Ledger.vue
+++ src/views/Ledger.vue
@@ -1,12 +1,12 @@
 <template>
-<div>
-  <Layout class-prefix="layout" >
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
-  </Layout>
-</div>
+  <div>
+    <Layout class-prefix="layout" >
+      <NumberPad/>
+      <Types/>
+      <Notes/>
+      <Tags :data-source.sync="tags"/>
+    </Layout>
+  </div>
 </template>
 
 <script lang="ts">
@@ -14,15 +14,12 @@ import NumberPad from '@/components/Ledger/NumberPad.vue';
 import Types from '@/components/Ledger/Types.vue';
 import Notes from '@/components/Ledger/Notes.vue';
 import Tags from '@/components/Ledger/Tags.vue';
-export default {
-  name: 'Ledger',
-  components: {Tags, Notes, Types, NumberPad},
-  data(){
-    return{
-      tags:['衣','食','住','行','股票']
-    }
-  }
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+@Component({components: {Tags, Notes, Types, NumberPad}})
+export default class Ledger extends Vue{
+  tags = ['衣', '食', '住', '行', '股票'];
+}
 </script>
 
 <style lang="scss">
