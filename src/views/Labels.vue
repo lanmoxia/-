0aef4a40962d41754d679090c3a9e873747ea010
lifeng <506076395@qq.com>
2022-03-31 13:48:08 +0800

    router-link 重定向：点击标签跳转编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,14 @@
 <template>
 <div>
   <Layout>
-    <ol class="tags">
-    <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+    <router-link class="tag"
+        v-for="tag in tags" :key="tag.id"
+        :to="`/labels/edit/${tag.id}`">
       <span>{{tag.name}}</span>
       <Icons name="right"/>
-    </li>
-    </ol>
+    </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -43,7 +45,7 @@ export default class Labels extends Vue{
 .tags{
   background-color: white;
   font-size: 16px;
-  >li{
+  > .tag{
     min-height: 44px;
     display: flex;
     align-items: center;
