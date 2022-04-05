df91d38fa920e7973afc700fe6fdd0295c509182
lifeng <506076395@qq.com>
2022-04-06 02:38:39 +0800

    tagStore && recordStore

---
+++ src/store/tagStore.ts
@@ -0,0 +1,22 @@
+import tagListModel from '@/models/tagListModel';
+
+export default {
+    tagList : tagListModel.fetch(),
+    findTag(id: string){ // 这里不能使用箭头函数 this 会指向 window
+        return this.tagList.filter(t => t.id === id)[0]
+    },
+    createTag : (name: string) => {
+        const message = tagListModel.create(name)
+        if(message === 'duplicated'){
+            window.alert('标签名重复了')
+        }else if(message === 'success'){
+            window.alert('添加成功')
+        }
+    },
+    removeTag : (id: string) =>{
+        return tagListModel.remove(id); // 简化
+    },
+    updateTag : (id: string, name: string) => {
+        return tagListModel.update(id, name)
+    }
+}
