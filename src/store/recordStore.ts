a28271c14b04ffe4820a8dde8a97eaf1b5ae8012
lifeng <506076395@qq.com>
2022-04-06 15:36:57 +0800

    与 tagStore 结构保持一致 初始化 tagStore 中的 recordList

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,25 +1,25 @@
 import clone from '@/lib/clone';
 const localStorageKeyName = 'recordList';
-let data: RecordItem[] | undefined = undefined;
 
 const recordStore =  {
-    recordList : data,
+    recordList: [] as RecordItem[],
     // 外边需要触发就把 fetch 暴漏出去
     fetchRecords(){
-        data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-        return data
+        this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.recordList
     },
     // 外边需要触发就把 save 暴漏出去
     saveRecords(){
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList))
     },
-    createRecord : (record: RecordItem) =>{
+    createRecord(record: RecordItem){
         // 深拷贝
         const record2: RecordItem = clone(record)
         // 生成日期
         record2.createdAt = new Date()
         // 不使用深拷贝 | 这里是引用的地址 每次数据更新了都会覆盖之前的
-        data && data.push(record2)
+        //this.recordList && this.recordList.push(record2)
+        this.recordList?.push(record2) // 可选链语法
         recordStore.saveRecords()
     }
 }
