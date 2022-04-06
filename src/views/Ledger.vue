10f00857a9ea91ea496c0357bbf89640c1d08baa
lifeng <506076395@qq.com>
2022-04-06 18:58:42 +0800

    关于值与地址的传递

--- src/views/Ledger.vue
+++ src/views/Ledger.vue
@@ -8,6 +8,7 @@
         <FormItem filter-name="备注" placeholder="请在这里输入" @update:value="onUpdateNotes"/>
       </div>
       <Tags/>
+      {{count}} <button @click="add">+1</button>
     </Layout>
   </div>
 </template>
@@ -25,9 +26,14 @@ import store from '@/store/index2';
 @Component({components: {Tags, FormItem, Types, NumberPad}})
 export default class Ledger extends Vue{
   // 获取数据
-  recordList = store.recordList
+  count= store.count // 只是把 0 复制到 count store.count 变化了并不影响左边 count 的值
+  recordList = store.recordList // 无名对象的地址的复制到 recordList 对象更改 引用的同一个地址的 recordList 也会跟着变化
   record: RecordItem = {
     tags:[], notes:'', type:'-', amount: 0
+  }
+  // 点击按钮并没有反应 这是因为 count= store.count 是传的值
+  add(){
+    store.addCount()
   }
   onUpdateNotes(value: string) {this.record.notes = value}
   saveRecord(){store.createRecord(this.record)}
