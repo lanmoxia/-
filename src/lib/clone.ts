87ac0b40c5e1b66a32914a7f29e00d6494de31a8
lifeng <506076395@qq.com>
2022-04-09 01:17:27 +0800

    排序

--- src/lib/clone.ts
+++ src/lib/clone.ts
@@ -1,4 +1,4 @@
-function clone(data: any){
+function clone<X>(data: X):X{ // data 什么类型 返回值就是什么类型 第一个 X 是定义
     return JSON.parse(JSON.stringify(data))
 }
 export default clone
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -6,7 +6,7 @@
         <!-- group 一组 === array-->
         <!-- 数组没有 key 只能用 index 代替 key-->
         <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{group.title}}</h3>
+          <h3 class="title">{{beautify(group.title)}}</h3>
           <ol>
             <li v-for="item in group.items" :key="item.id"
                 class="record"
@@ -20,27 +20,7 @@
       </ol>
   </Layout>
 </template>
-<style scoped lang="scss">
-%item {
-  padding: 8px 16px;
-  line-height: 24px;
-  display: flex;
-  justify-content: space-between;
-  align-content: center;
-}
-.title {
-  @extend %item;
-}
-.record {
-  background: white;
-  @extend %item;
-}
-.notes {
-  margin-right: auto;
-  margin-left: 16px;
-  color: #999;
-}
-</style>
+
 
 <script lang="ts">
 import Vue from 'vue';
@@ -48,10 +28,17 @@ import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constant/intervalList';
 import recordTypeList from '@/constant/recordTypeList';
+import dayjs from 'dayjs'
+import clone from '@/lib/clone';
+
 @Component({
   components: {Tabs}
 })
 export default class Statistics extends Vue{
+  type = '-';
+  interval = 'day';
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
   tagString(tags: Tag[]) {
     return tags.length === 0 ? '无' : tags.join(',');
   }
@@ -60,23 +47,34 @@ export default class Statistics extends Vue{
   }
   get result() {
     const {recordList} = this
-    type HashTableValue = {title:string,items: RecordList[]}
+    type HashTableValue = { title: string, items: RecordItem[] }
     // 如何生成一个空的对象的类型
-    const hashTable: {[key: string]: HashTableValue} = {} // hashTable 的 key 是字符串 类型是 RecordItem 扩展的类型
-    for (let i = 0; i < this.recordList.length; i++) {
-      const [date, time] = recordList[i].createdAt!.split('T')
-      hashTable[date] = hashTable[date] || {title:date, items: []}
-      hashTable[date].items.push(recordList[i])
-    }
-    return hashTable
+    //const hashTable: {key: string, items: RecordItem[]}[];// 对象遍历不一定按照顺序 所以它必须是一个数组
+    //console.log(recordList.map(i => i.createdAt));
+    // 使用 sort 来排序 需要给它两个值 一个表达式 `> = <` 三种可能
+    // 由于 sort 改变原数组 这里我们只能clone clone的返回值可以通过 recordList 来推断出来 sort 每一项 TS 也能推出来
+    const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+    //console.log(recordList.map(i => i.createdAt));
+    return [];
   }
   beforeCreate(){// 这里要使用 before 不然 hashTable 会出现空数组的问题
     this.$store.commit('fetchRecords')
   }
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
+  beautify(string: string){
+    const day = dayjs(string)
+    const now = dayjs()
+    if(day.isSame(now, 'day')){
+      return '今天'
+    }else if(day.isSame(now.subtract(1,'day'), 'day')){
+      return '昨天'
+    }else if(day.isSame(now.subtract(2,'day'), 'day')){
+      return '前天'
+    }else if(day.isSame(now, 'year')){
+      return day.format('M月D日')
+    }else{
+      return day.format('YYY年M月D日')
+    }
+  }
 }
 </script>
 
@@ -95,5 +93,24 @@ export default class Statistics extends Vue{
     .interval-tabs-item {
       height: 48px;
     }
+  }
+  %item {
+    padding: 8px 16px;
+    line-height: 24px;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+  }
+  .title {
+    @extend %item;
+  }
+  .record {
+    background: white;
+    @extend %item;
+  }
+  .notes {
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
   }
 </style>
