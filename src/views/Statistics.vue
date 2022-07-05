74e7a94613f338f45ac40764b5bf772a756ac3f1
lifeng <506076395@qq.com>
2022-07-06 00:32:43 +0800

    recordList 改为 groupedList

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -52,8 +52,10 @@ export default class Statistics extends Vue{
     const array = []
     for(let i = 0; i <= 29; i++){
       const dateString = day(today).subtract(i, 'day').format('YYYY-MM-DD')
-      const found = _.find(this.recordList, {createdAt : dateString})
-      array.push({key: dateString, value: found ? found.amount : 0})
+      // recordList 既有支出还有收入 可能会有重复的 date
+      // 这里要使用 groupedList 他是剥离过的数据
+      const found = _.find(this.groupedList, {title : dateString})
+      array.push({key: dateString, value: found ? found.total : 0})
     }
     // 顺序反了 排下序
     array.sort((a,b) => {
