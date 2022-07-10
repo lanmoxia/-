108df8f66f10518ccc8509fd64fd4c5f4d04d792
lifeng <506076395@qq.com>
2022-07-10 12:06:29 +0800

    lint

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,4 +1,8 @@
 // 这里是一个默认的全局的类型声明 TS 看到 d.ts 就认为是声明
+type Tag = {
+    id: string
+    name: string
+}
 
 type RecordItem = {
     tags: Tag[]
@@ -8,21 +12,6 @@ type RecordItem = {
     createdAt?: string// JSON 不支持 Date 所以这里类型要变更下
 }
 
-type Tag = {
-    id: string
-    name: string
-}
-
-// 这里定义好类型
-// 等号左边是输入类型 右边是输出类型
-type tagListModel = {
-    data: Tag[] // 这里的 data 是一个 Tag 对象的数组
-    create: (name: string) =>  'success' | 'duplicated' // 只能是这两个字符串 可以防止手贱写错会提示(联合类型)
-    fetch: () => Tag[] // // 这里的 data 是一个 Tag 对象的数组
-    update: (id: string , name: string) => 'success' | 'not fount' | 'duplicated'
-    remove: (id:string) => boolean
-    save: () => void
-}
 type RootState ={
     // createRecordError: Error | null,
     createTagError: Error | null,
--- src/store/index.ts
+++ src/store/index.ts
@@ -14,8 +14,51 @@ const store =  new Vuex.Store({
     currentTag: undefined, // ① 第一步先定义当前 currentTag 默认 undefined
   } as RootState,
   mutations: {
-    setCurrentTag(state, id: string){
-      state.currentTag =  state.tagList.filter(t => t.id === id)[0]
+    fetchRecords(state){
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    },
+    createRecord(state, record: RecordItem){
+      // 深拷贝
+      const record2 = clone(record)
+      // 生成日期
+      record2.createdAt = record2.createdAt || new Date().toISOString()
+      // 不使用深拷贝 | 这里是引用的地址 每次数据更新了都会覆盖之前的
+      //this.recordList && this.recordList.push(record2)
+      state.recordList.push(record2)
+      store.commit('saveRecords')
+    },
+    saveRecords(state){
+      window.localStorage.setItem('recordList', JSON.stringify(state.recordList))
+    },
+
+    createTag(state, name: string){
+      state.createTagError = null
+      // 创建一个 tag 如果重复
+      const names = state.tagList.map(item =>item.name)
+      if(names.indexOf(name) >= 0 ){
+        state.createTagError = new Error('tag name duplicated');
+        return
+      }
+      // 如果没有重复 创建一个 id 然后把 id 和 name 放到 tagList 中保存
+      const id = createId().toString()
+      state.tagList.push({id, name:name}) //{id: id, name: name}
+      store.commit('saveTags')
+    },
+    removeTag(state,id: string){
+      let index = -1;
+      for(let i=0; i<state.tagList.length;i++){
+        if(state.tagList[i].id === id){
+          index = i;
+          break;
+        }
+      }
+      if(index >= 0){
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back()
+      }else{
+        window.alert('删除失败')
+      }
     },
     updateTag(state, payload:{id: string, name: string}){// 这里值接受除了 state 的第二个参数 只能用对象 对象名一般是 payload
       const {id,name} = payload
@@ -34,37 +77,8 @@ const store =  new Vuex.Store({
         }
       }
     },
-    removeTag(state,id: string){
-      let index = -1;
-      for(let i=0; i<state.tagList.length;i++){
-        if(state.tagList[i].id === id){
-          index = i;
-          break;
-        }
-      }
-      if(index >= 0){
-        state.tagList.splice(index, 1);
-        store.commit('saveTags');
-        router.back()
-      }else{
-        window.alert('删除失败')
-      }
-    },
-    fetchRecords(state){
-      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
-    },
-    createRecord(state, record: RecordItem){
-      // 深拷贝
-      const record2 = clone(record)
-      // 生成日期
-      record2.createdAt = record2.createdAt || new Date().toISOString()
-      // 不使用深拷贝 | 这里是引用的地址 每次数据更新了都会覆盖之前的
-      //this.recordList && this.recordList.push(record2)
-      state.recordList.push(record2)
-      store.commit('saveRecords')
-    },
-    saveRecords(state){
-      window.localStorage.setItem('recordList', JSON.stringify(state.recordList))
+    saveTags(state){
+      return window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
     },
     fetchTags(state){
       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
@@ -75,21 +89,8 @@ const store =  new Vuex.Store({
         store.commit('createTag', '行')
       }
     },
-    createTag(state, name: string){
-      state.createTagError = null
-      // 创建一个 tag 如果重复
-      const names = state.tagList.map(item =>item.name)
-      if(names.indexOf(name) >= 0 ){
-        state.createTagError = new Error('tag name duplicated');
-        return
-      }
-      // 如果没有重复 创建一个 id 然后把 id 和 name 放到 tagList 中保存
-      const id = createId().toString()
-      state.tagList.push({id, name:name}) //{id: id, name: name}
-      store.commit('saveTags')
-    },
-    saveTags(state){
-      return window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
+    setCurrentTag(state, id: string){
+      state.currentTag =  state.tagList.filter(t => t.id === id)[0]
     }
   }
 })
